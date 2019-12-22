988722a2d34ff53c17952db2aae6dc03ccb70125
MurDoDePar <dominique.mura@gmail.com>
2019-12-22 06:12:15 +0100

    first commit

---
+++ test.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Dec 22 05:49:12 2019
+
+@author: MurDoDePar
+"""
+
+v='zzzzzzzz'
+
+print(f'test {v}')
---
+++ test2.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Nov 15 13:53:23 2019
+
+@author: MurDoDePar
+"""
+
+import pandas as pd
+
+pd.read_csv("./data/SRD_2018-12-31.txt")
